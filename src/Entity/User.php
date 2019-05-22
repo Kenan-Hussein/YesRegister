be39507bb1f2e15689617b5d3a869ff427525c3e
Kenan-Hussein <in.ctrl.x@hotmail.com>
2019-05-22 20:38:27 +0300

    add @Group(api)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -14,16 +15,19 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups("api")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups("api")
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups("api")
      */
     private $roles = [];
 
