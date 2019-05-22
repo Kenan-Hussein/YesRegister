303774931171bcbd1613683b80fc348ed005e696
Kenan-Hussein <in.ctrl.x@hotmail.com>
2019-05-22 18:34:04 +0300

    Save user to DB

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,59 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/register", name="api_register", methods={"POST"})
      */
-    public function index()
+    public function register(ObjectManager $om, UserPasswordEncoderInterface $passwordEncoder, Request $request)
     {
+        $user = new User();
+
+        $email                  = $request->request->get("email");
+        $password               = $request->request->get("password");
+        $passwordConfirmation   = $request->request->get("password_confirmation");
+
+        //Array holds errors
+        $errors = [];
+
+        //Check if password does not match the confirmation
+        if($password != $passwordConfirmation)
+        {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+        //check that the password is at least 6 characters long
+        if(strlen($password) < 6)
+        {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+
+        if(!$errors)
+        {
+            //Encrypt the password
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+
+            //Setter methods to set the email and password properties of the user
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+
+            //Save new user
+            $om->persist($user);
+            $om->flush();
+
+            //return the User object as json
+            return $this->json([
+                'user' => $user
+            ]);
+        }
+
+        //return the $errors as json if there are any with status code
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+            'errors' => $errors
+        ], 400);
     }
 }
